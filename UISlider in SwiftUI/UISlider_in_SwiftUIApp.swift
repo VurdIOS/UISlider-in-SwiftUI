3b2d092bec544c1fa34c4bfe39d22eac1eb79f68
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2023-05-01 12:31:52 +0300

    Initial Commit

---
+++ UISlider in SwiftUI/UISlider_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UISlider_in_SwiftUIApp.swift
+//  UISlider in SwiftUI
+//
+//  Created by Камаль Атавалиев on 01.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct UISlider_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
